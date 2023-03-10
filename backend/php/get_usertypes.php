cf27ce7dc95a39c8ef47909f77936075797844a5
aliynajjar <ali.y.alnajjar@gmail.com>
2023-03-10 13:29:53 +0200

    Added registration & members API

--- backend/php/get_usertypes.php
+++ backend/php/get_usertypes.php
@@ -1,10 +1,7 @@
 <?php
 include('connection.php');
 
-$user_type_id = $_POST['user_type_id'];
-
-$query = $mysqli->prepare('SELECT id,first_name,last_name,email FROM users WHERE usertype_id=?');
-$query->bind_param('i', $user_type_id);
+$query = $mysqli->prepare('SELECT * FROM user_types');
 $query->execute();
 $array=$query->get_result();
 
--- backend/php/registration.php
+++ backend/php/registration.php
@@ -6,6 +6,7 @@ $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$user_type= $_POST['user_type'];
 
 $check_email =  $mysqli->prepare('select email from users where email=?');
 $check_email->bind_param('s', $email);
@@ -18,8 +19,8 @@ $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 if ($email_exists > 0) {
     $response['status'] = "failed";
 } else {
-    $query =  $mysqli->prepare('insert into users(first_name,last_name,email,password) values(?,?,?,?)');
-    $query->bind_param('ssss', $first_name, $last_name, $email, $hashed_password);
+    $query =  $mysqli->prepare('insert into users(first_name,last_name,email,password,usertype_id) values(?,?,?,?,?)');
+    $query->bind_param('ssssi', $first_name, $last_name, $email, $hashed_password,$user_type);
     $query->execute();
     $response['status'] = "success";
 }
